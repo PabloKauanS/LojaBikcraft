e7bab99ab1435eb3fcd7a75b743d8dcefc7d79fe
Pablo Kauan <pablokauanoficial@gmail.com>
2022-12-08 07:21:00 -0300

    javascript-add

---
+++ js/script.js
@@ -0,0 +1,62 @@
+//ativar links do menu
+const links = document.querySelectorAll(".header-nav a");
+function AtivarLink(link) {
+  const url = window.location.href;
+  const href = link.href;
+  if (url.includes(href)) {
+    link.classList.add("ativo");
+  }
+}
+links.forEach(AtivarLink);
+
+// ativar items do orçamento
+
+const parametros = new URLSearchParams(window.location.search);
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+}
+parametros.forEach(ativarProduto);
+
+//perguntas frequentes
+
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+  resposta.classList.toggle("ativa");
+  const ativa = resposta.classList.contains("ativa");
+  pergunta.setAttribute("aria-expand", ativa);
+}
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListener("click", ativarPergunta);
+}
+perguntas.forEach(eventosPerguntas);
+
+// galeria de bicicletas
+
+const galeria = document.querySelectorAll(".bicicleta-imagens img");
+const galeriaContainer = document.querySelector(".bicicleta-imagens");
+
+function trocarImg(event) {
+  const img = event.currentTarget;
+  const media = window.matchMedia("(min-width: 1000px)").matches;
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
+}
+
+function eventosGaleria(img) {
+  img.addEventListener("click", trocarImg);
+}
+
+galeria.forEach(eventosGaleria);
+
+//animação
+if (window.SimpleAnime) {
+  new SimpleAnime();
+}
